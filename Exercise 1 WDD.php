fd262726d6b92016b0d8b276ede9aa1edfe2664c
John Reeves Yecla <89110784+jreevesy@users.noreply.github.com>
2022-09-30 19:47:36 +0800

    Add files via upload

---
+++ Exercise 1 WDD.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+<title> Exercise 1 WDD </title>
+</head>
+<body>
+<center>
+
+
+
+<?php
+
+	//declare variables
+	$name = "John Reeves G. Yecla";
+	$college_program = "Bachelor of Science in Computer Engineering";
+	$year = "3rd Year";
+	$section = "Web Design and Development Lab - 58043";
+	$string = "Today, I will learn PHP Programming.";
+	
+	//print variables
+	echo "$name";
+	echo "<br>";
+	echo "$college_program";
+	echo "<br>";
+	echo "$year";
+	echo "<br>";
+	echo "$section";
+	echo "<br>";
+	echo "$string";
+	echo "<br>";
+	
+?>
+
+
+</body>
+</html>
